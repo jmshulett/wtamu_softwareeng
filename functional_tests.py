74617015bae51a3d25b03de353f620c4677f5f94
john hulett <jphulett1@buffS.wtamu.edu>
2020-02-17 20:09:19 -0600

    Refactor home page view to use a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,22 +1,43 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVistorTest(unittest.TestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
+		#self.browser.implicitly_wait(3)
 		
 	def tearDown(self):
 		self.browser.quit()
 		
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
+		#self.browser.get('http://localhost')
 		
 		self.assertIn('To-Do', self.browser.title)
 		self.fail('Finish the test!')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 		
-		#[...rest of comments as before]
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get.attribute('placeholder'),
+			'Enter a to-do item'
+			)
+			
+		inputbox.send_keys('By peacock feathers')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_element_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+			)
+			
 		
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
